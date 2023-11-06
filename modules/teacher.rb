9d45966953380276e1eaad84f756e661d8276244
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-11-06 15:36:05 +0100

    main added

--- modules/teacher.rb
+++ modules/teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(name: "Unknown", age: 0, parent_permission: true, specialization: "Unknown")
+  def initialize(name: 'Unknown', age: 0, parent_permission: true, specialization: 'Unknown')
     super(name: name, age: age, parent_permission: parent_permission)
     @specialization = specialization
   end
