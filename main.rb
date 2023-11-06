79353c692cba1d7e4b833d508917b1651225b716
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-11-06 19:15:08 +0100

    remove attr accessor from classroom and specialization

--- main.rb
+++ main.rb
@@ -12,12 +12,11 @@ puts "Age: #{person.age}"
 puts "Can use services? #{person.can_use_services? && 'Yes!'}"
 puts '--------------------------'
 
-student = Student.new(16, name: 'Ramos', classroom: 'Math')
+student = Student.new(16, 'Math', name: 'Ramos')
 puts 'Student Info:'
 puts "ID: #{student.id}"
 puts "Name: #{student.name}"
 puts "Age: #{student.age}"
-puts "Classroom: #{student.classroom}"
 puts "Can use services? #{student.can_use_services? && 'Yes!'}"
 puts "Play hooky: #{student.play_hooky}"
 puts '--------------------------'
@@ -27,5 +26,4 @@ puts 'Teacher Info:'
 puts "ID: #{teacher.id}"
 puts "Name: #{teacher.name}"
 puts "Age: #{teacher.age}"
-puts "Specialization: #{teacher.specialization}"
 puts "Can use services? #{teacher.can_use_services? && 'Yes!'}"
--- modules/student.rb
+++ modules/student.rb
@@ -1,9 +1,7 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
-
-  def initialize(age, name: 'Unknown', parent_permission: true, classroom: 'Unknown')
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
