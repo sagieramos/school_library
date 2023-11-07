730a8f6e29f190afb9fb378527b9bdb7e4a0886b
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-11-07 15:31:58 +0100

    check if you managed to decorate your person

--- modules/decorator.rb
+++ modules/decorator.rb
@@ -1,29 +1,24 @@
-requie "./nameable.rb"
+require_relative 'nameable'
 
 class BaseDecorator < Nameable
-    def initialize(nameable)
-        @nameable = nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
 
 class CapitalizeDecorator < BaseDecorator
-    def correct_name
-        super.correct_name.capitalize 
-    end
+  def correct_name
+    super.capitalize
+  end
 end
 
 class TrimmerDecorator < BaseDecorator
-    def correct_name 
-        trimmed_name = super.correct_name[0, 10]
-        
-        if trimmed_name.length < super.correct_name.length
-            trimmed_name
-        else
-            super.correct_name
-        end
-    end
+  def correct_name
+    super[0, 10]
+  end
 end
